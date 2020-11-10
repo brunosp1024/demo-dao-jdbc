563d45017da596049bfd4df9af6a7f895954e03b
brunosp1024 <brunosalvador145@gmail.com>
2020-11-10 14:50:03 -0300

    update implementation Seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -14,36 +14,44 @@ import model.entities.Seller;
 public class Program {
 
 	public static void main(String[] args) throws ParseException {
-		
+
 		Locale.setDefault(Locale.US);
-		
+
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		Seller sel = sellerDao.findById(3);
 		System.out.println(sel);
-		
+
 		System.out.println("----------------------------------------------------------------------");
-		
+
 		Department department = new Department(2, null);
 		List<Seller> list = sellerDao.findByDepartment(department);
-		for(Seller obj : list) {
+		for (Seller obj : list) {
 			System.out.println(obj);
 		}
-		
+
 		System.out.println("----------------------------------------------------------------------");
-		
+
 		list = sellerDao.findAll();
-		for(Seller obj : list) {
+		for (Seller obj : list) {
 			System.out.println(obj);
 		}
-		
+
 		System.out.println("----------------------------------------------------------------------");
-		
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-		
-		Seller seller = new Seller(null, "Goku", "goku@gmail.com", sdf.parse("13/04/1991"), 2500.0, new Department(3, null));
+
+		/*SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+		Seller seller = new Seller(null, "Goku", "goku@gmail.com", sdf.parse("13/04/1991"), 2500.0,
+				new Department(3, null));
 		sellerDao.insert(seller);
-		System.out.println("Inserted: new Id: " + seller.getId());
-		
+		System.out.println("Inserted: new Id: " + seller.getId());*/
+
+		System.out.println("----------------------------------------------------------------------");
+
+		Seller sell = sellerDao.findById(1);
+		sell.setName("Maria bendita");
+		sellerDao.update(sell);
+		System.out.println("Update completed!");
+
 	}
 
 }
